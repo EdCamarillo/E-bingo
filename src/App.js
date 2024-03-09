c3420d08ff49cafb9aa95198f77290954d778b69
EdCamarillo <edwardjohn.camarillo@cit.edu>
2024-03-09 22:27:39 +0800

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import GameBoard from './GameBoard';  
+import Card from './Card';
+import React, {useState} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Lobby from './Lobby';
+import { Button } from '@mui/material';
+import Game from './Game';
+
+function App() {
+
+  return (
+    <div className='game'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Lobby />} />
+          <Route path="/game/:gameCode" element={<Game />} />
+        </Routes>
+      </Router>
+    
+    </div>  
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,79 @@
+import React, { 
+    useEffect, 
+    useState 
+} from 'react';
+import { Button } from '@mui/material';
+import axios from 'axios';
+
+function Card({ gameCode }) {
+  const [cardData, setCardData] = useState(null);
+  const [isWinner, setIsWinner] = useState(false);
+  const [checkWinClicked, setCheckWinClicked] = useState(false);
+
+  useEffect(() => {
+    const fetchCardData = async () => {
+      try {
+        const response = await axios.get(`http://www.hyeumine.com/getcard.php?bcode=${gameCode}`);
+        setCardData(response.data);
+      } catch (error) {
+        console.error('Error fetching card data: ', error);
+      }
+    };
+
+    fetchCardData();
+  }, [gameCode]);
+
+  const checkWin = async (playcardToken) => {
+    try {
+      const response = await axios.get(`http://www.hyeumine.com/checkwin.php?playcard_token=${playcardToken}`);
+      setIsWinner(response.data === 1);
+    } catch (error) {
+      console.error('Error checking win: ', error);
+    }
+    
+    setCheckWinClicked(true);
+  };
+
+  return (
+    <div>
+      {cardData && (
+        <div>
+        {checkWinClicked && !isWinner && (
+        <div style={{textAlign: 'center'}}>Better luck next time!</div>
+          )}
+          {isWinner && (
+            <div style={{textAlign: 'center'}}>BINGO! Congratulations!</div>
+          )}
+          <table className={`bingo-card ${isWinner ? 'winner' : ''}`}>
+            <thead>
+              <tr>
+                <th>B</th>
+                <th>I</th>  
+                <th>N</th>
+                <th>G</th>
+                <th>O</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(cardData.card).map(([letter, numbers]) => (
+                <tr key={letter}>
+                  {numbers.map((number, index) => (
+                    <td key={index} className={`bingo-number-box ${isWinner? 'winning-number' : ''}`}>
+                      {number}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Button onClick={() => checkWin(cardData.playcard_token)} 
+          variant="contained" 
+          color="primary" 
+          style={{marginLeft:'30%', height: '25px'}}>Check Win</Button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Card;
